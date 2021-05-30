f7e91600221a1db0db8841a159d5bf541dc5fb90
dipen <dipendrathapa044@gmail.com>
2021-05-30 18:14:37 +0545

    Add to learn git

--- errorcheck.go
+++ errorcheck.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"errors"
+	"log"
+)
+
+func main()  {
+	m,n:=testdividebyzero(12,0)
+	log.Println(m,n)
+
+}
+func testdividebyzero(x,y float32) (float32,error)  {
+	var result float32
+	if y==0 {
+		return result,errors.New("division by zero error")
+     }
+    result=x/y
+    return result,nil
+ }
